cfb3a8cac7af42985dd9d5b46d98e0df8981d311
naqiboqi <naqihasn.2080@outlook.com>
2025-02-28 03:09:56 -0500

    Add init.py for map breakdown models (province, area, region, country)

--- core/__init__.py
+++ core/__init__.py
@@ -1,3 +1,4 @@
 from .colors import load_province_colors, load_tag_colors
 from .map import apply_colors_to_map
 from .provinces import extract_provinces_block, map_provinces_to_countries
+from .stats import analyze_map, calculate_all_country_area
