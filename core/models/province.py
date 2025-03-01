381f547bf2feabb459a4f73e217d240390bb8ff2
naqiboqi <naqihasn.2080@outlook.com>
2025-03-01 03:59:50 -0500

    Made province a dataclass

--- core/models/province.py
+++ core/models/province.py
@@ -1,10 +1,33 @@
+from dataclasses import dataclass
+from enum import Enum
+from typing import Optional
 
 
+class ProvinceType(Enum):
+    OWNED = "owned"
+    NATIVE = "native"
+    SEA = "sea"
+    WASTELAND = "wasteland"
 
+
+@dataclass
 class EUProvince:
-    def __init__(self, prov_id: int, name: str):
-        self.prov_id = prov_id
-        self.name = name
+    province_id: int
+    name: str
+    province_type: ProvinceType
+    owner: Optional[str] = None
+    capital: Optional[str] = None
+    culture: Optional[str] = None
+    religion: Optional[str] = None
+    base_tax: Optional[int] = None
+    base_production: Optional[int] = None
+    base_manpower: Optional[int] = None
+    native_size: Optional[int] = None
+    patrol: Optional[int] = None
+
+    @classmethod
+    def from_dict(cls, data: dict[str, str]):
+        return cls(**data)
 
     def __str__(self):
-        return f"Province: {self.name} with ID {self.prov_id}"
+        return f"Province: {self.name} with ID {self.province_id}"
