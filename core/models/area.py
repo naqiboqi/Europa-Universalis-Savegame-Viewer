1bd5eca7c62e92e2f01e10c81b11131d2cbfb6dc
naqiboqi <naqihasn.2080@outlook.com>
2025-03-01 04:48:53 -0500

    Add internal area id to area.py for matching to regions

--- core/models/area.py
+++ core/models/area.py
@@ -6,8 +6,9 @@ from . import EUProvince
 
 @dataclass
 class EUArea:
+    area_id: str
     name: str
     provinces: dict[int, EUProvince]|list[int]
 
     def __str__(self):
-        return f"The area: {self.name}, containing the provinces: {self.provinces}"
+        return f"The area: {self.name} (internal id: {self.area_id}), containing the provinces: {self.provinces}"
