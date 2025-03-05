e952700dbb7726bbec8a56d943d16977b46daeee
naqiboqi <naqihasn.2080@outlook.com>
2025-03-05 01:19:59 -0500

    Fix circular import in models/init.py

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,5 +1,5 @@
+from .province import EUProvince, ProvinceType, ProvinceTypeColor
 from .area import EUArea
 from .mapmode import MapMode
-from .province import EUProvince, ProvinceType, ProvinceTypeColor
 from .region import EURegion
 from .world import EUWorldData
