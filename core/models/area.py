075eb62126a88047cf12dbfa843d7bf5a9320c12
naqiboqi <naqihasn.2080@outlook.com>
2025-03-05 20:56:07 -0500

    Add method to generate name from id

--- core/models/area.py
+++ core/models/area.py
@@ -10,5 +10,10 @@ class EUArea:
     name: str
     provinces: dict[int, EUProvince]|list[int]
 
+    @classmethod
+    def name_from_id(cls, area_id: str):
+        name_split = area_id.removesuffix("area").replace("_", " ").split()
+        return " ".join(name.capitalize() for name in name_split)
+
     def __str__(self):
         return f"The area: {self.name} (internal id: {self.area_id}), containing the provinces: {self.provinces}"
