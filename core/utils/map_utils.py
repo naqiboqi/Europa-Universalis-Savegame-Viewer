277f132d66b6ed26bf453152a89506378d55e386
naqiboqi <naqihasn.2080@outlook.com>
2025-03-07 18:21:44 -0500

    Add static method to seed colors

--- core/utils/map_utils.py
+++ core/utils/map_utils.py
@@ -1,3 +1,4 @@
+import hashlib
 
 
 
@@ -24,3 +25,11 @@ class MapUtils:
         if image_dim < canvas_dim:
             return (canvas_dim - image_dim) // 2
         return MapUtils.clamp(offset, canvas_dim - image_dim, 0)
+
+    @staticmethod
+    def seed_color(name: str):
+        hash_value = int(hashlib.md5(name.encode("utf-8")).hexdigest(), 16)
+        r = (hash_value >> 16) & 0xFF
+        g = (hash_value >> 8) & 0xFF
+        b = hash_value & 0xFF
+        return (r, g, b)
