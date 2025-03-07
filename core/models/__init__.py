5baa24d73f0bd0bc032a8fcf3091985c1d85b99f
naqiboqi <naqihasn.2080@outlook.com>
2025-03-07 18:35:39 -0500

    Add fix_name method to add proper capitalization

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,4 +1,5 @@
 from .province import EUProvince, ProvinceType, ProvinceTypeColor
 from .area import EUArea
+from .country import EUCountry
 from .mapmode import MapMode
 from .region import EURegion
--- core/models/country.py
+++ core/models/country.py
@@ -1,3 +1,5 @@
+import re
+
 from dataclasses import dataclass
 from typing import Optional
 
@@ -8,6 +10,12 @@ class EUCountry:
     tag: str
     tag_color: tuple[int]
     name: Optional[str] = None
+
+    @staticmethod
+    def fix_name(country_name: str):
+        name = country_name.replace('countries/', '')
+        formatted_name = re.sub(r'([a-z])([A-Z])', r'\1 \2', name)
+        return formatted_name.title()
 
     def __str__(self):
         return f"The country of {self.name} (TAG: {self.tag})"
