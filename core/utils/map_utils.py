a60abafdb442f7d40704881b606ee49339b0003d
naqiboqi <naqihasn.2080@outlook.com>
2025-03-08 05:08:25 -0500

    Update seed color method to have more variation

--- core/utils/map_utils.py
+++ core/utils/map_utils.py
@@ -1,4 +1,6 @@
+import colorsys
 import hashlib
+from random import Random
 
 
 
@@ -28,8 +30,13 @@ class MapUtils:
 
     @staticmethod
     def seed_color(name: str):
-        hash_value = int(hashlib.md5(name.encode("utf-8")).hexdigest(), 16)
-        r = (hash_value >> 16) & 0xFF
-        g = (hash_value >> 8) & 0xFF
-        b = hash_value & 0xFF
-        return (r, g, b)
+        hash_value = int(hashlib.sha256(name.encode("utf-8")).hexdigest(), 16)
+
+        random = Random(hash_value)
+
+        hue = random.uniform(0, 1)
+        saturation = random.uniform(0.6, 0.9)
+        brightness = random.uniform(0.75, 0.85)
+
+        r, g, b = colorsys.hsv_to_rgb(hue, saturation, brightness)
+        return (int(r * 255), int(g * 255), int(b * 255))
