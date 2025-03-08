ba1d6d7d07a4deb2658ca28e3a67dacb9d0a8950
naqiboqi <naqihasn.2080@outlook.com>
2025-03-08 03:54:13 -0500

    Add properties to determine the area type

--- core/models/area.py
+++ core/models/area.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 
-from . import EUProvince
+from . import EUProvince, ProvinceType
 
 
 
@@ -14,10 +14,28 @@ class EUArea:
     def from_dict(cls, data: dict[str, str]):
         return cls(**data)
 
+    @property
+    def is_land_area(self):
+        return any(
+            province.province_type == ProvinceType.NATIVE or 
+            province.province_type == ProvinceType.OWNED for province in self)
+
+    @property
+    def is_sea_area(self):
+        return any(province.province_type == ProvinceType.SEA for province in self)
+
+    @property
+    def is_wasteland_area(self):
+        return any(province.province_type == ProvinceType.WASTELAND for province in self)
+
     @classmethod
     def name_from_id(cls, area_id: str):
         name_split = area_id.removesuffix("area").replace("_", " ").split()
         return " ".join(name.capitalize() for name in name_split)
+
+    @property
+    def pixel_locations(self):
+        return set(loc for province in self for loc in province.pixel_locations)
 
     def __iter__(self):
         for province in self.provinces.values():
