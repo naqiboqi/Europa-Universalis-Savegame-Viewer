6a3d15ee3865cea839cf9d49305562fe89d8812f
naqih <na828@drexel.edu>
2025-03-09 01:42:27 -0500

    Remove Mapmodeselector class

--- core/map_paint.py
+++ core/map_paint.py
@@ -11,34 +11,11 @@ from .world import EUWorldData
 
 
 
-class MapModeSelector:
-    def __init__(self):
-        self.map_mode = None
-
-    def select_map_mode(self):
-        modes = [mode.value for mode in MapMode]
-        while not self.map_mode:
-            print("\n".join([f'{i}. {mode.capitalize()}' for i, mode in enumerate(modes, 1)]) + "\n")
-
-            try:
-                map_mode = int(input("Select a map mode to view (enter the number): \n"))
-            except ValueError:
-                print("Please enter a valid selection.\n")
-                continue
-
-            if not 1 <= map_mode <= len(modes):
-                print("Please enter a valid selection.\n")
-                continue
-
-            self.map_mode = MapMode(modes[map_mode - 1])
-
-
-
 class MapPainter:
     def __init__(self, colors: EUColors, world_data: EUWorldData):
         self.colors = colors
         self.world_data = world_data
-        self.selector = MapModeSelector()
+        self.map_mode = MapMode.POLITICAL
         self.map_modes = {
             MapMode.POLITICAL: self.draw_map_political,
             MapMode.AREA: self.draw_map_area,
@@ -47,33 +24,44 @@ class MapPainter:
             MapMode.RELIGION: self.draw_map_religion
         }
 
-        self.handler: MapEventHandler = None
         self.canvas: tk.Canvas = None
+        self.canvas_id: int = None
+        self.handler: MapEventHandler = None
         self.hover_label: tk.Label = None
         self.quit_button: tk.Button = None
         self.root: tk.Tk = None
         self.tk_image: ImageTk.PhotoImage = None
         self.world_image: Image.Image = None
 
+    def set_map_mode(self, map_mode: MapMode):
+        self.map_mode = map_mode
+        self.draw_map()
+
     def draw_map(self):
         print("Drawing map....")
-        #draw_map_mode = self.map_modes.get(self.selector.map_mode, self.draw_map_political)
+
         self.world_image = self.world_data.world_image
-        map_pixels = self.draw_map_area()
+        draw_method = self.map_modes.get(self.map_mode, self.draw_map_political)
+        map_pixels = draw_method()
 
-        world_image = Image.fromarray(map_pixels)
-        self.world_image = world_image
-
+        self.world_image = Image.fromarray(map_pixels)
         if not self.root:
             self.root = tk.Tk()     
             self.root.title("Map Viewer")
 
-            self.canvas = tk.Canvas(self.root, width=1200, height=900)
-            self.canvas.pack(fill=tk.BOTH, expand=True)
-
             self.tk_image = ImageTk.PhotoImage(self.world_image)
-            self.canvas.create_image(0, 0, anchor=tk.NW, image=self.tk_image)
-            self.canvas.config(scrollregion=self.canvas.bbox(tk.ALL))
+
+            button_frame = tk.Frame(self.root)
+            button_frame.pack(side=tk.TOP, fill=tk.X)
+            for map_mode in self.map_modes.keys():
+                button = tk.Button(
+                    button_frame, 
+                    text=map_mode.name, 
+                    command=lambda m=map_mode: self.set_map_mode(m))
+                button.pack(fill=tk.X, padx=5, pady=5)
+            
+            self.canvas = tk.Canvas(self.root, width=1200, height=600)
+            self.canvas.pack(fill=tk.BOTH, expand=True)
 
             self.quit_button = tk.Button(self.root, text="Quit", command=self.root.destroy)
             self.quit_button.pack()
@@ -83,13 +71,30 @@ class MapPainter:
 
             self.handler = MapEventHandler(
                 canvas=self.canvas,
-                map_mode=self.selector.map_mode,
+                map_mode=self.map_mode,
                 world_image=self.world_image,
                 hover_label=self.hover_label, 
                 provinces=self.world_data.provinces)
             self.canvas.bind("<Motion>", self.handler.on_map_hover)
 
-        self.root.mainloop()
+            self.root.mainloop()
+        else:
+            self.tk_image = ImageTk.PhotoImage(self.world_image)
+            if self.canvas_id:
+                self.canvas.itemconfig(self.canvas_id, image=self.tk_image)
+            else:
+                x_center = (self.canvas.winfo_width() - self.world_image.width) // 2
+                y_center = (self.canvas.winfo_height() - self.world_image.height) // 2
+                self.canvas_id = self.canvas.create_image(x_center, y_center, anchor=tk.CENTER, image=self.tk_image)
+
+            self.canvas.config(scrollregion=self.canvas.bbox(tk.ALL))
+            self.handler = MapEventHandler(
+                canvas=self.canvas,
+                map_mode=self.map_mode,
+                world_image=self.world_image,
+                hover_label=self.hover_label, 
+                provinces=self.world_data.provinces)
+            self.canvas.bind("<Motion>", self.handler.on_map_hover)
 
     def draw_map_political(self):
         world_provinces = self.world_data.provinces
