5167232f149b6753025ff12aeed65ffbd1383d6d
naqiboqi <naqihasn.2080@outlook.com>
2025-03-10 05:57:10 -0400

    Removed mapeventhandler and all references to tkinter because it is bad

--- core/__init__.py
+++ core/__init__.py
@@ -1,5 +1,4 @@
 from .colors import EUColors
 from .stats import analyze_map, calculate_all_country_area
-from .map_eventhandler import MapEventHandler
 from .map_paint import MapPainter
 from .world import EUWorldData
--- core/map_paint.py
+++ core/map_paint.py
@@ -1,11 +1,9 @@
 import math
 import numpy as np
-import tkinter as tk
 
 from PIL import Image, ImageTk
 from typing import Optional
 from .colors import EUColors
-from .map_eventhandler import MapEventHandler
 from .models import EUArea, EUProvince, MapMode, ProvinceType, ProvinceTypeColor, EURegion
 from .utils import MapUtils
 from .world import EUWorldData
@@ -28,98 +26,6 @@ class MapPainter:
             MapMode.DEVELOPMENT: self.draw_map_development,
             MapMode.RELIGION: self.draw_map_religion
         }
-
-        self.canvas: tk.Canvas = None
-        self.canvas_id: int = None
-        self.handler: MapEventHandler = None
-        self.hover_label: tk.Label = None
-        self.quit_button: tk.Button = None
-        self.root: tk.Tk = None
-        self.tk_image: ImageTk.PhotoImage = None
-        self.world_image: Image.Image = None
-
-    def create_handler(self, offset_x: Optional[int]=None, offset_y: Optional[int]=None):
-        if not self.handler:
-            self.handler = MapEventHandler(
-                canvas=self.canvas,
-                canvas_id=self.canvas_id,
-                hover_label=self.hover_label, 
-                map_mode=self.map_mode,
-                world_data=self.world_data,
-                world_image=self.world_image,
-                offset_x=offset_x,
-                offset_y=offset_y)
-        else:
-            data = {
-                "canvas": self.canvas,
-                "canvas_id": self.canvas_id,
-                "hover_label": self.hover_label,
-                "map_mode": self.map_mode,
-                "world_data": self.world_data,
-                "world_image": self.world_image,
-            }
-            self.handler.update(data)
-        self.canvas.bind("<Motion>", self.handler.on_map_hover)
-
-    def set_map_mode(self, map_mode: MapMode):
-        if not self.map_mode == map_mode:
-            self.map_mode = map_mode
-            self.draw_map()
-
-    def create_canvas(self):
-        self.root = tk.Tk()     
-        self.root.title("Map Viewer")
-
-        self.tk_image = ImageTk.PhotoImage(self.world_image)
-
-        button_frame = tk.Frame(self.root)
-        button_frame.pack(side=tk.TOP, fill=tk.X)
-        for map_mode in self.map_modes.keys():
-            button = tk.Button(
-                button_frame, 
-                text=map_mode.name, 
-                command=lambda m=map_mode: self.set_map_mode(m))
-            button.pack(fill=tk.X, padx=5, pady=5)
-
-        self.canvas = tk.Canvas(self.root, width=CANVAS_WIDTH, height=CANVAS_HEIGHT)
-        self.canvas.pack(fill=tk.BOTH, expand=True)
-
-        self.quit_button = tk.Button(self.root, text="Quit", command=self.root.destroy)
-        self.quit_button.pack()
-
-        self.hover_label = tk.Label(self.root, text="Choose a Province", bg="white")
-        self.hover_label.pack()
-
-        offset_x = (CANVAS_WIDTH - self.world_image.width) // 2
-        offset_y = (CANVAS_HEIGHT - self.world_image.height) // 2
-        return offset_x, offset_y
-
-    def draw_map(self):
-        print("Drawing map....")
-
-        self.world_image = self.world_data.world_image
-        draw_method = self.map_modes.get(self.map_mode, self.draw_map_political)
-        map_pixels = draw_method()
-
-        self.world_image = Image.fromarray(map_pixels)
-        if not self.root:
-            offset_x, offset_y = self.create_canvas()
-            self.canvas_id = self.canvas.create_image(offset_x, offset_y, anchor=tk.NW, image=self.tk_image)
-            self.canvas.config(scrollregion=self.canvas.bbox(tk.ALL))
-            self.create_handler(offset_x=offset_x, offset_y=offset_y)
-
-            self.root.mainloop()
-        else:
-            self.tk_image = ImageTk.PhotoImage(self.world_image)
-            if self.canvas_id:
-                self.canvas.itemconfig(self.canvas_id, image=self.tk_image)
-            else:
-                offset_x = (CANVAS_WIDTH - self.world_image.width) // 2
-                offset_y = (CANVAS_HEIGHT - self.world_image.height) // 2
-                self.canvas_id = self.canvas.create_image(offset_x, offset_y, anchor=tk.NW, image=self.tk_image)
-
-            self.canvas.config(scrollregion=self.canvas.bbox(tk.ALL))
-            self.create_handler()
 
     def draw_map_political(self):
         world_provinces = self.world_data.provinces
@@ -178,7 +84,7 @@ class MapPainter:
 
                 x_coords, y_coords = zip(*region_pixels)
                 map_pixels[y_coords, x_coords] = region_color
-        
+
         wasteland_pixels = set()
         for province in self.world_data.areas.get("wasteland_area"):
             wasteland_pixels.update(province.pixel_locations)
