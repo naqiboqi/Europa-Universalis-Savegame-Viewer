302468ea051bb22fdc0da0e52d8e3d33a2549e2d
naqiboqi <naqihasn.2080@outlook.com>
2025-03-11 01:26:14 -0400

    Start implementation for displaying map

--- core/map_display.py
+++ core/map_display.py
@@ -1,7 +1,7 @@
 import FreeSimpleGUI as fsg
 import io
 
-from PIL import Image
+from PIL import Image, ImageTk
 from . import MapPainter
 from .models import MapMode
 
@@ -10,7 +10,13 @@ from .models import MapMode
 class MapDisplayer:
     def __init__(self, painter: MapPainter):
         self.painter = painter
-        
+
+        self.pan_x = 0
+        self.pan_y = 0
+        self.dragging = False
+        self.start_x = 0
+        self.start_y = 0
+
     def image_to_bytes(self, image: Image.Image):
         with io.BytesIO() as output:
             image.save(output, format="PNG")
@@ -21,31 +27,3 @@ class MapDisplayer:
 
         map_image = self.painter.draw_map()
         max_width, max_height = 1200, 800
-        
-        scale = min(max_width / map_image.width, max_height / map_image.height)
-        new_size = (int(map_image.width * scale), int(map_image.height * scale))
-        map_image = map_image.resize(new_size, Image.Resampling.LANCZOS)
-        
-        map_bytes = self.image_to_bytes(map_image)
-
-        layout = [
-            [fsg.Image(data=map_bytes, key="-IMAGE-")],
-            [fsg.Button(mode.value.capitalize(), key=mode.value.capitalize()) for mode in self.painter.map_modes]
-        ]
-
-        print("Loading map....")
-        window = fsg.Window("EU Map Viewer", layout, finalize=True)
-        window.move_to_center()
-
-        while True:
-            event, values = window.read(timeout=20)
-
-            if event in (fsg.WIN_CLOSED, "Exit"):
-                break
-
-            if event in {mode.value.capitalize() for mode in self.map_modes}:
-                self.painter.map_mode = MapMode[event.upper()]
-                image = self.image_to_bytes(self.painter.draw_map().resize(new_size, Image.Resampling.LANCZOS))
-                window["-IMAGE-"].update(data=image)
-
-        window.close()
