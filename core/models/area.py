740592ee5480202354fec971eca0483f21967720
naqiboqi <naqihasn.2080@outlook.com>
2025-03-18 02:48:28 -0400

    Add docstrings

--- core/models/area.py
+++ core/models/area.py
@@ -1,3 +1,9 @@
+"""
+This module defines EUArea, which represents a collection of provinces in Europa Universalis IV.
+"""
+
+
+
 from dataclasses import dataclass
 
 from . import EUProvince, ProvinceType
@@ -6,21 +12,46 @@ from . import EUProvince, ProvinceType
 
 @dataclass
 class EUArea:
+    """Represents an area on the map.
+    
+    Attributes:
+        area_id (str): The area's in-game identifier.
+        name (str): The area's name.
+        provinces (dict[int, EUProvince]): A mapping of province IDs to EUProvinces
+            that belong to the area.
+    """
     area_id: str
     name: str
-    provinces: dict[int, EUProvince]|list[int]
+    provinces: dict[int, EUProvince]
 
     @classmethod
-    def from_dict(cls, data: dict[str, str]):
+    def from_dict(cls, data: dict[str, str|dict]):
+        """Builds the area from a dictionary."""
         return cls(**data)
 
     @classmethod
     def name_from_id(cls, area_id: str):
+        """Gets the area name from the area ID for displaying.
+        
+        Args:
+            area_id (str): The area ID.
+
+        Returns:
+            str: The display name.
+        """
         name_split = area_id.removesuffix("area").replace("_", " ").split()
         return " ".join(name.capitalize() for name in name_split)
 
     @property
     def bounding_box(self):
+        """Gets the bounding box for the area.
+        
+        The bounding box is defined as the inclusive limits of its x and y values, by
+        checking its contained provinces.
+        
+        Returns:
+            tuple[int]: The bounding box.
+        """
         locations = self.pixel_locations
         if not locations:
             return None
@@ -37,20 +68,27 @@ class EUArea:
 
     @property
     def is_land_area(self):
+        """Checks if the area contains any land provinces. An area can only contain one type
+            of province"""
         return any(
             province.province_type == ProvinceType.NATIVE or 
             province.province_type == ProvinceType.OWNED for province in self)
 
     @property
     def is_sea_area(self):
+        """Checks if the area contains any sea provinces. An area can only contain one type
+            of province"""
         return any(province.province_type == ProvinceType.SEA for province in self)
 
     @property
     def is_wasteland_area(self):
+        """Checks if the area contains any wasteland provinces. An area can only contain one type
+            of province."""
         return any(province.province_type == ProvinceType.WASTELAND for province in self)
 
     @property
     def pixel_locations(self):
+        """Returns the set of x, y coordinates occupied by the area."""
         return set(loc for province in self for loc in province.pixel_locations)
 
     def __iter__(self):
--- core/models/region.py
+++ core/models/region.py
@@ -1,3 +1,9 @@
+"""
+This module defines EURegion, which represents a collection of areas in Europa Universalis IV.
+"""
+
+
+
 from dataclasses import dataclass
 
 from . import EUArea
@@ -6,21 +12,46 @@ from . import EUArea
 
 @dataclass
 class EURegion:
+    """Represents a region on the map.
+    
+    Attributes:
+        region_id (str): The regions's in-game identifier.
+        name (str): The regions's name.
+        areas (dict[str, EUArea]): A mapping of area IDs to EUAreas
+            that belong to the region.
+    """
     region_id: str
     name: str
     areas: dict[str, EUArea]
 
     @classmethod
     def from_dict(cls, data: dict):
+        """Builds the region from a dictionary."""
         return cls(**data)
 
     @classmethod
     def name_from_id(cls, region_id: str):
+        """Gets the region name from the region ID for displaying.
+        
+        Args:
+            region_id (str): The region ID.
+
+        Returns:
+            str: The display name.
+        """
         name_split = region_id.removesuffix("region").replace("_", " ").split()
         return " ".join(name.capitalize() for name in name_split)
 
     @property
     def bounding_box(self):
+        """Gets the bounding box for the region.
+        
+        The bounding box is defined as the inclusive limits of its x and y values, by
+        checking its contained provinces.
+        
+        Returns:
+            tuple[int]: The bounding box.
+        """
         locations = self.pixel_locations
         if not locations:
             return None
@@ -37,14 +68,19 @@ class EURegion:
 
     @property
     def is_land_region(self):
+        """Checks if the region contains any land areas. A region can only contain one type
+            of province"""
         return any(area.is_land_area for area in self)
 
     @property
     def is_sea_region(self):
+        """Checks if the region contains any sea areas. Aregion only contain one type
+            of province"""
         return any(area.is_sea_area for area in self)
 
     @property
     def pixel_locations(self):
+        """Returns the set of x, y coordinates occupied by the region."""
         return set(loc for area in self for loc in area.pixel_locations)
 
     def __iter__(self):
