10ac203a6857457da92570e434992f75c6631c8c
naqiboqi <naqihasn.2080@outlook.com>
2025-03-18 03:06:55 -0400

    Add docstrings

--- core/models/country.py
+++ core/models/country.py
@@ -1,3 +1,9 @@
+"""
+This module defines EUCountry, which represents a country in
+Europa Universalis IV.
+"""
+
+
 import re
 
 from dataclasses import dataclass
@@ -7,12 +13,22 @@ from typing import Optional
 
 @dataclass
 class EUCountry:
+    """Represents a country on the map.
+    
+    Attributes:
+        tag (str): The three-letter identifier used internally by EU4.
+        tag_color (tuple[int, int, int]): The RGB color of the country.
+        name (Optional[str]): The name of the country. 
+            Is optional as some countries have dynamic names
+            (and I am not sure how to get them from savefiles yet).
+    """
     tag: str
     tag_color: tuple[int]
     name: Optional[str] = None
 
     @staticmethod
     def fix_name(country_name: str):
+        """Attempts to apply proper capitalization to the country's name."""
         name = country_name.replace('countries/', '')
         formatted_name = re.sub(r'([a-z])([A-Z])', r'\1 \2', name)
         return formatted_name.title()
