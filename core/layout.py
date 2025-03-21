c451c50551e0aee946f5e54ac9ea1b8959b0480a
naqiboqi <naqihasn.2080@outlook.com>
2025-03-21 13:32:31 -0400

    Add method to create framed text

--- core/layout.py
+++ core/layout.py
@@ -20,6 +20,9 @@ class Layout:
     BUTTON_FG = "#394d66"
     GREEN_TEXT = "#2b8334"
 
+    @staticmethod
+    def add_border(layout: list, border_color, inner_width, outer_width, pad, expand_x, expand_y):
+        pass
 
     @staticmethod
     def create_button(
@@ -49,10 +52,30 @@ class Layout:
             visible=visible)
 
     @staticmethod
-    def build_layout():
-        layout = []
-        
-        return layout
+    def create_framed_text(
+        content: str,
+        content_color: str,
+        frame_background_color: str,
+        font: tuple[int, str, int]=("Georgia", 12, "bold")):
+        return sg.Frame("", [
+            [sg.Text(
+                content, 
+                font=font, 
+                text_color=content_color, 
+                background_color=frame_background_color)]
+        ], background_color=frame_background_color, element_justification="left", pad=(5, 5))
+
+    @staticmethod
+    def create_text_with_header():
+        pass
+
+    @staticmethod
+    def create_map_info_header():
+        pass
+
+    @staticmethod
+    def create_hover_output():
+        pass
 
     @staticmethod
     def create_window_header():
@@ -83,6 +106,7 @@ class Layout:
                             font=("Georgia", 14),
                             border_width=3,
                             pad=(5, 5))],
+
                     ], justification="center", element_justification="center", expand_x=True, pad=(5, 5), background_color=Layout.LIGHT_FRAME_BG)]
                 ], expand_x=True, relief=sg.RELIEF_SUNKEN, border_width=2, background_color=Layout.GOLD_FRAME_LOWER, pad=(5, 5))]
             ], expand_x=True, relief=sg.RELIEF_RAISED, border_width=2, background_color=Layout.GOLD_FRAME_UPPER, pad=(15, 15))
@@ -91,90 +115,104 @@ class Layout:
     def create_search_column():
         return sg.Column([
             [sg.Frame("", [
-                [sg.Push(background_color=Layout.LIGHT_FRAME_BG), sg.Frame("", [
-                    [sg.Text("Search", font=("Georgia", 12, "bold"), text_color=Layout.LIGHT_TEXT, background_color=Layout.RED_BANNER_BG)]
-                ], pad=(30, 5), background_color=Layout.RED_BANNER_BG),
-                sg.Push(background_color=Layout.LIGHT_FRAME_BG)],
-                [sg.Input(size=(30, 1), key="-SEARCH-", font=("Georgia", 12), enable_events=True, text_color=Layout.LIGHT_TEXT, background_color=Layout.DARK_FRAME_BG)],
+                [sg.Push(Layout.LIGHT_FRAME_BG), 
+                    Layout.create_framed_text("Search", Layout.LIGHT_TEXT, Layout.RED_BANNER_BG),
+                sg.Push(Layout.LIGHT_FRAME_BG)],
 
+                [sg.Input(size=(30, 1), key="-SEARCH-", font=("Georgia", 12), enable_events=True, text_color=Layout.LIGHT_TEXT, background_color=Layout.DARK_FRAME_BG)],
                 [sg.Checkbox("Exact Matches?", key="-EXACT_MATCH-", enable_events=True, font=("Georgia", 11), text_color=Layout.LIGHT_TEXT, background_color=Layout.LIGHT_FRAME_BG)],
 
                 [sg.Push(background_color=Layout.LIGHT_FRAME_BG), sg.Frame("", [
                     [sg.Listbox(values=[], size=(30, 5), key="-RESULTS-", enable_events=True, font=("Segoe UI", 12), visible=False, text_color=Layout.LIGHT_TEXT, background_color=Layout.SUNKEN_FRAME_BG)],
                     [Layout.create_button("Go to", "-GOTO-", Layout.LIGHT_TEXT, Layout.BUTTON_BG, visible=False)],
                     [Layout.create_button("Clear", "-CLEAR-", Layout.LIGHT_TEXT, Layout.BUTTON_BG, visible=False)]
-                    ], background_color=Layout.LIGHT_FRAME_BG, element_justification="center", border_width=0),
+                ], background_color=Layout.LIGHT_FRAME_BG, element_justification="center", border_width=0),
                 sg.Push(background_color=Layout.LIGHT_FRAME_BG)]
+
             ], expand_y=True, pad=(10, 10), relief=sg.RELIEF_GROOVE, background_color=Layout.LIGHT_FRAME_BG, border_width=5, title_color=Layout.LIGHT_TEXT)]
-            ], vertical_alignment="top", pad=(10, 10), expand_y=True, justification="left", background_color=Layout.DARK_FRAME_BG)
+        ], vertical_alignment="top", pad=(10, 10), expand_y=True, justification="left", background_color=Layout.DARK_FRAME_BG)
+
+    @staticmethod
+    def create_geo_info_frame():
+        return sg.Frame("", [
+            [sg.Column([
+                [sg.Text("Province:", font=("Georgia", 14, "bold"), text_color=Layout.LIGHT_TEXT, background_color=Layout.TOP_BANNER_BG),
+                sg.Text("", key="-INFO_NAME-", font=("Georgia", 14, "bold"), text_color=Layout.LIGHT_TEXT, background_color=Layout.TOP_BANNER_BG)],
+
+                [sg.Text("Capital:", font=("Georgia", 12), text_color=Layout.LIGHT_TEXT, background_color=Layout.TOP_BANNER_BG),
+                sg.Text("", key="-INFO_CAPITAL-", font=("Georgia", 12), text_color=Layout.LIGHT_TEXT, background_color=Layout.TOP_BANNER_BG)],
+
+            ], background_color=Layout.TOP_BANNER_BG, element_justification="left", expand_x=True),
+
+            sg.Column([
+                [sg.Text("Area:", font=("Georgia", 14, "bold"), text_color=Layout.LIGHT_TEXT, background_color=Layout.TOP_BANNER_BG),
+                sg.Text("", key="-INFO_PROVINCE_AREA-", font=("Georgia", 14, "bold"), text_color=Layout.LIGHT_TEXT, background_color=Layout.TOP_BANNER_BG)],
+
+                [sg.Text("Region:", font=("Georgia", 12), text_color=Layout.LIGHT_TEXT, background_color=Layout.TOP_BANNER_BG),
+                sg.Text("", key="-INFO_PROVINCE_REGION-", font=("Georgia", 12), text_color=Layout.LIGHT_TEXT, background_color=Layout.TOP_BANNER_BG)],
+
+            ], background_color=Layout.TOP_BANNER_BG, element_justification="right", expand_x=True)]
+        ], background_color=Layout.TOP_BANNER_BG, relief=sg.RELIEF_RAISED, border_width=4, title_color=Layout.LIGHT_TEXT, pad=(5, 5), expand_x=True)
+
+    @staticmethod
+    def create_development_info_frame():
+        return sg.Frame("", [
+            [sg.Text("Tax:", font=("Georgia", 12), text_color=Layout.LIGHT_TEXT, background_color=Layout.DARK_FRAME_BG),  
+            sg.Text("", key="-INFO_BASE_TAX-", font=("Georgia", 12), text_color=Layout.GREEN_TEXT, background_color=Layout.DARK_FRAME_BG, size=(10, 1)),  
+
+            sg.Text("Production:", font=("Georgia", 12), text_color=Layout.LIGHT_TEXT, background_color=Layout.DARK_FRAME_BG),  
+            sg.Text("", key="-INFO_BASE_PRODUCTION-", font=("Georgia", 12), text_color=Layout.GREEN_TEXT, background_color=Layout.DARK_FRAME_BG, size=(10, 1)),  
+
+            sg.Text("Manpower:", font=("Georgia", 12), text_color=Layout.LIGHT_TEXT, background_color=Layout.DARK_FRAME_BG),  
+            sg.Text("", key="-INFO_BASE_MANPOWER-", font=("Georgia", 12), text_color=Layout.GREEN_TEXT, background_color=Layout.DARK_FRAME_BG, size=(10, 1))]
+        ], background_color=Layout.DARK_FRAME_BG, pad=(5, 5), relief=sg.RELIEF_SUNKEN, border_width=3, title_color=Layout.LIGHT_TEXT)
+
+    @staticmethod
+    def create_demographic_info_frame():
+        return sg.Frame("", [
+            [sg.Text("Cored by:", font=("Georgia", 12), text_color=Layout.LIGHT_TEXT, background_color=Layout.DARK_FRAME_BG)],
+            [sg.Frame("", [
+                [sg.Text("", key="-INFO_OWNER-", font=("Georgia", 12), text_color=Layout.GREEN_TEXT, background_color=Layout.SUNKEN_FRAME_BG, size=(20, 1))]
+            ], background_color=Layout.SUNKEN_FRAME_BG, pad=(5, 5), border_width=0)],
+
+            [sg.Text("Culture:", font=("Georgia", 12), text_color=Layout.LIGHT_TEXT, background_color=Layout.DARK_FRAME_BG)],
+            [sg.Frame("", [
+                [sg.Text("", key="-INFO_CULTURE-", font=("Georgia", 12), text_color=Layout.GREEN_TEXT, background_color=Layout.SUNKEN_FRAME_BG, size=(20, 1))]
+            ], background_color=Layout.SUNKEN_FRAME_BG, pad=(5, 5), border_width=0)],
+
+            [sg.Text("Religion:", font=("Georgia", 12), text_color=Layout.LIGHT_TEXT, background_color=Layout.DARK_FRAME_BG)],
+            [sg.Frame("", [
+                [sg.Text("", key="-INFO_RELIGION-", font=("Georgia", 12), text_color=Layout.GREEN_TEXT, background_color=Layout.SUNKEN_FRAME_BG, size=(20, 1))]
+            ], background_color=Layout.SUNKEN_FRAME_BG, pad=(5, 5), border_width=0)]
+
+        ], background_color=Layout.DARK_FRAME_BG, pad=(5, 5), relief=sg.RELIEF_SUNKEN, border_width=4, expand_x=True)
 
     @staticmethod
     def create_province_info_column():
         return sg.Column([
             [sg.Frame("", [
-                [sg.Frame("", [
-                    [sg.Column([
-                        [sg.Text("Province:", font=("Georgia", 14, "bold"), text_color=Layout.LIGHT_TEXT, background_color=Layout.TOP_BANNER_BG),
-                        sg.Text("", key="-INFO_NAME-", font=("Georgia", 14, "bold"), text_color=Layout.LIGHT_TEXT, background_color=Layout.TOP_BANNER_BG)],
-
-                        [sg.Text("Capital:", font=("Georgia", 12), text_color=Layout.LIGHT_TEXT, background_color=Layout.TOP_BANNER_BG),
-                        sg.Text("", key="-INFO_CAPITAL-", font=("Georgia", 12), text_color=Layout.LIGHT_TEXT, background_color=Layout.TOP_BANNER_BG)],
-                    ], background_color=Layout.TOP_BANNER_BG, element_justification="left", expand_x=True),
-
-                    sg.Column([
-                        [sg.Text("Area:", font=("Georgia", 14, "bold"), text_color=Layout.LIGHT_TEXT, background_color=Layout.TOP_BANNER_BG),
-                        sg.Text("", key="-INFO_PROVINCE_AREA-", font=("Georgia", 14, "bold"), text_color=Layout.LIGHT_TEXT, background_color=Layout.TOP_BANNER_BG)],
-
-                        [sg.Text("Region:", font=("Georgia", 12), text_color=Layout.LIGHT_TEXT, background_color=Layout.TOP_BANNER_BG),
-                        sg.Text("", key="-INFO_PROVINCE_REGION-", font=("Georgia", 12), text_color=Layout.LIGHT_TEXT, background_color=Layout.TOP_BANNER_BG)],
-                    ], background_color=Layout.TOP_BANNER_BG, element_justification="right", expand_x=True)]
-                ], background_color=Layout.TOP_BANNER_BG, relief=sg.RELIEF_RAISED, border_width=4, title_color=Layout.LIGHT_TEXT, pad=(5, 5), expand_x=True)],
+                [Layout.create_geo_info_frame()],
 
                 [sg.Frame("", [
                     [sg.Text("Development", font=("Georgia", 12, "bold"), text_color=Layout.LIGHT_TEXT, background_color=Layout.SECTION_BANNER_BG)]
                 ], background_color=Layout.SECTION_BANNER_BG, pad=(5, 5), element_justification="left")],
-                [sg.Frame("", [
-                    [sg.Text("Tax:", font=("Georgia", 12), text_color=Layout.LIGHT_TEXT, background_color=Layout.DARK_FRAME_BG),  
-                    sg.Text("", key="-INFO_BASE_TAX-", font=("Georgia", 12), text_color=Layout.GREEN_TEXT, background_color=Layout.DARK_FRAME_BG, size=(10, 1)),  
 
-                    sg.Text("Production:", font=("Georgia", 12), text_color=Layout.LIGHT_TEXT, background_color=Layout.DARK_FRAME_BG),  
-                    sg.Text("", key="-INFO_BASE_PRODUCTION-", font=("Georgia", 12), text_color=Layout.GREEN_TEXT, background_color=Layout.DARK_FRAME_BG, size=(10, 1)),  
-
-                    sg.Text("Manpower:", font=("Georgia", 12), text_color=Layout.LIGHT_TEXT, background_color=Layout.DARK_FRAME_BG),  
-                    sg.Text("", key="-INFO_BASE_MANPOWER-", font=("Georgia", 12), text_color=Layout.GREEN_TEXT, background_color=Layout.DARK_FRAME_BG, size=(10, 1))]
-                ], background_color=Layout.DARK_FRAME_BG, pad=(5, 5), relief=sg.RELIEF_SUNKEN, border_width=3, title_color=Layout.LIGHT_TEXT),
-                sg.Push(background_color=Layout.LIGHT_FRAME_BG)],  
+                [Layout.create_development_info_frame(), sg.Push(background_color=Layout.LIGHT_FRAME_BG)],  
 
                 [sg.Frame("", [
                     [sg.Text("Demographics", font=("Georgia", 12, "bold"), text_color=Layout.LIGHT_TEXT, background_color=Layout.SECTION_BANNER_BG, pad=(1, 1))]
                 ], background_color=Layout.SECTION_BANNER_BG, pad=(1, 1), element_justification="left")],
-                [sg.Frame("", [
-                    [sg.Text("Cored by:", font=("Georgia", 12), text_color=Layout.LIGHT_TEXT, background_color=Layout.DARK_FRAME_BG)],
-                    [sg.Frame("", [
-                        [sg.Text("", key="-INFO_OWNER-", font=("Georgia", 12), text_color=Layout.GREEN_TEXT, background_color=Layout.SUNKEN_FRAME_BG, size=(20, 1))]
-                    ], background_color=Layout.SUNKEN_FRAME_BG, pad=(5, 5), border_width=0)],
 
-                    [sg.Text("Culture:", font=("Georgia", 12), text_color=Layout.LIGHT_TEXT, background_color=Layout.DARK_FRAME_BG)],
-                    [sg.Frame("", [
-                        [sg.Text("", key="-INFO_CULTURE-", font=("Georgia", 12), text_color=Layout.GREEN_TEXT, background_color=Layout.SUNKEN_FRAME_BG, size=(20, 1))]
-                    ], background_color=Layout.SUNKEN_FRAME_BG, pad=(5, 5), border_width=0)],
+                [Layout.create_demographic_info_frame(), sg.Push(background_color=Layout.LIGHT_FRAME_BG)]
 
-                    [sg.Text("Religion:", font=("Georgia", 12), text_color=Layout.LIGHT_TEXT, background_color=Layout.DARK_FRAME_BG)],
-                    [sg.Frame("", [
-                        [sg.Text("", key="-INFO_RELIGION-", font=("Georgia", 12), text_color=Layout.GREEN_TEXT, background_color=Layout.SUNKEN_FRAME_BG, size=(20, 1))]
-                    ], background_color=Layout.SUNKEN_FRAME_BG, pad=(5, 5), border_width=0)]
-
-                ], background_color=Layout.DARK_FRAME_BG, pad=(5, 5), relief=sg.RELIEF_SUNKEN, border_width=4, expand_x=True),
-                sg.Push(background_color=Layout.LIGHT_FRAME_BG)]
             ], key="-PROVINCE_INFO-", pad=(10, 10), relief=sg.RELIEF_GROOVE, background_color=Layout.LIGHT_FRAME_BG, border_width=5, title_color=Layout.LIGHT_TEXT)]
         ], vertical_alignment="top", pad=(10, 10), expand_y=True, background_color=Layout.DARK_FRAME_BG)
 
-
-
-
     @staticmethod
-    def create_map_canvas(canvas_size: tuple[int, int], key: str):
-        return sg.Canvas(background_color="black", size=canvas_size, key=key, pad=(10, 10))
+    def create_map_canvas_frame(canvas_size: tuple[int, int], key: str):
+        return sg.Frame("", [
+            [sg.Canvas(background_color="black", size=canvas_size, key=key, pad=(10, 10))]
+        ], background_color=Layout.LIGHT_FRAME_BG, relief=sg.RELIEF_GROOVE, pad=(10, 10), border_width=5)
 
     @staticmethod
     def create_map_modes_frame(map_modes: dict):
@@ -183,12 +221,7 @@ class Layout:
             [Layout.create_button(mode.name, mode.value, Layout.LIGHT_TEXT, Layout.BUTTON_BG)
                 for mode in map_modes],
             [Layout.create_button("Reset View", "-RESET-", Layout.LIGHT_TEXT, Layout.BUTTON_BG)]
-            ], element_justification="center",
-            relief=sg.RELIEF_GROOVE,
-            pad=(10, 10),
-            background_color=Layout.DARK_FRAME_BG,
-            border_width=5,
-            title_color=Layout.LIGHT_TEXT)
+        ], element_justification="center", relief=sg.RELIEF_GROOVE, pad=(10, 10), background_color=Layout.DARK_FRAME_BG, border_width=5, title_color=Layout.LIGHT_TEXT)
 
     @staticmethod
     def get_layout(canvas_size: tuple[int, int], map_modes: dict):
@@ -196,18 +229,10 @@ class Layout:
             [Layout.create_window_header()],
 
             [sg.Frame("", [
-                [
-                    Layout.create_search_column(), Layout.create_province_info_column()
-                ]
+                [Layout.create_search_column(), Layout.create_province_info_column()]
             ], pad=(10, 10), relief=sg.RELIEF_GROOVE, background_color=Layout.LIGHT_FRAME_BG, border_width=5, expand_x=True)],
-            
-            [sg.Frame("", [
-                [
-                    Layout.create_map_canvas(canvas_size=canvas_size, key="-CANVAS-")
-                ]
-            ], background_color=Layout.LIGHT_FRAME_BG, relief=sg.RELIEF_GROOVE, pad=(10, 10), border_width=5)],
+
+            [Layout.create_map_canvas_frame(canvas_size=canvas_size, key="-CANVAS-")],
 
             [Layout.create_map_modes_frame(map_modes=map_modes)],
-
-            [sg.Text("", size=(1, 1), pad=(5, 5), background_color=Layout.LIGHT_FRAME_BG)]
         ]
