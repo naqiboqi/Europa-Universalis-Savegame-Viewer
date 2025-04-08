6c2272dd138891b6377df8c41793f4720fa90808
naqiboqi <naqihasn.2080@outlook.com>
2025-04-07 21:29:56 -0400

    Add docstrings

--- core/layouts/elements/sortable_table.py
+++ core/layouts/elements/sortable_table.py
@@ -8,12 +8,31 @@ from FreeSimpleGUI import Table
 
 
 class SortableTable(Table):
+    """A subclass of PySimpleGUI's Table that supports sorting by columns.
+    It enables sorting in ascending or descending order when a column is clicked.
+
+    Attributes:
+        _sort_column (int): The index of the currently sorted column.
+        _sort_reverse (bool): A flag indicating whether the sort order is reversed.
+    """
     def __init__(self, *args, **kwargs):
+        """Initializes the SortableTable with the given arguments and keyword arguments.
+
+        Args:
+            *args: Variable length argument list for the Table constructor.
+            **kwargs: Arbitrary keyword arguments for the Table constructor.
+        """
         super().__init__(*args, **kwargs)
         self._sort_column = None
         self._sort_reverse = False
 
     def sort_by_column(self, column: int):
+        """Sorts the table by the specified column index. Toggles the sort order
+        between ascending and descending each time the same column is clicked.
+
+        Args:
+            column (int): The index of the column to sort by.
+        """
         if self.Values is None or not self.Values:
             return
 
@@ -24,6 +43,9 @@ class SortableTable(Table):
             self._sort_reverse = False
 
         def try_parse(val: str|int|float):
+            """Attempts to convert the value to a float if possible, otherwise,
+            returns the string version of the value.
+            """
             try:
                 return float(val) if val not in [None, ""] else float('nan')
             except ValueError:
